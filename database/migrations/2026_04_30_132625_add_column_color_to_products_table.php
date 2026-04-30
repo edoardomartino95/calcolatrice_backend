79afcf6d4cf340b45a31adeaf9ecb53564b24c94
edoardomartino95 <2fs2hgh2@duck.com>
2026-04-30 15:44:55 +0200

    aggiunto i controller e i database ricorda di renderli persistenti chiedere a claude

--- database/migrations/2026_04_30_132625_add_column_color_to_products_table.php
+++ database/migrations/2026_04_30_132625_add_column_color_to_products_table.php
@@ -11,13 +11,8 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('products', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('slug');
-            $table->float('price');
-            $table->timestamps();
-            $table->softDeletes();
+        Schema::table('products', function (Blueprint $table) {
+            $table->string('color');
         });
     }
 
@@ -26,6 +21,9 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('products');
+        Schema::table('products', function (Blueprint $table) {
+            $table->dropColumn('color');
+            //
+        });
     }
 };
